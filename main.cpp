64d0ce0037cc68a30ba3c7bbf317786c222ae6e4
comp2 <comp2@lab.unesc.net>
2018-05-22 19:14:05 -0300

    init commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	
+	string exp = "(C+B)*C";
+	int cod = 1;
+	do {
+			
+	}while(cod != 0);
+	
+	system("pause");
+	return 0;
+}
